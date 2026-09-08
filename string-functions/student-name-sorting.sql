764f49d9ecdabb5ea603782ab179ade5d60c0008
salvylite <henryfurgeson@gmail.com>
2026-09-08 11:24:10 -0500

    Implement student name sorting query
    Add SQL query to filter and sort students by name and ID

--- string-functions/student-name-sorting.sql
+++ string-functions/student-name-sorting.sql
@@ -1,1 +1,15 @@
+/*
+Challenge: Higher Than 75 Marks
+Source: HackerRank
+Concepts: Filtering, string functions, multi-column sorting
 
+Task:
+Return students who scored above 75.
+Sort by the final three characters of their name.
+If those characters match, sort by student ID.
+*/
+
+SELECT name
+FROM Students
+WHERE marks > 75
+ORDER BY RIGHT(name, 3) ASC, id ASC;
